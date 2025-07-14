c820315d3fd1875c3bbd8a40ce400efef342a291
Fahmida Nimra <fahmidanimra@gmail.com>
2025-07-14 13:06:50 +0600

    set up mongodb and enviroment variable

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 require('dotenv').config()
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json())
 
 
+
+const uri =
+    `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.g5skvf8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
